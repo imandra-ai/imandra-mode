1a8e57689589c0d3b9ded0038a36e1eec409393a
Nicola Mometto <brobronsa@gmail.com>
2023-07-17 13:05:42 +0100

    feat: add imandra-mode-ocamlformat

---
+++ imandra-mode-ocamlformat.el
@@ -0,0 +1,8 @@
+(require 'imandra-mode)
+(require 'ocamlformat)
+
+(defun imandra--ocamlformat-before-save ()
+  (interactive)
+  (when (eq major-mode 'imandra-mode) (ocamlformat)))
+
+(add-hook 'before-save-hook 'imandra--ocamlformat-before-save)
