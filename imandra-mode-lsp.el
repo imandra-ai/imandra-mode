2e0d2a020c4a82681d007bb7a445c25bc8e57110
Nicola Mometto <brobronsa@gmail.com>
2023-07-17 12:56:45 +0100

    feat: move lsp into `imandra-mode-lsp`

---
+++ imandra-mode-lsp.el
@@ -0,0 +1,29 @@
+(require 'lsp-mode)
+(require 'imandra-mode)
+
+(defgroup imandra-lsp nil
+  "LSP support for Imandra"
+  :group 'lsp-mode)
+
+(defcustom imandra-lsp-command
+  "imandra-lsp"
+  "Path to the Imandra language server binary"
+  :group 'imandra-lsp
+  :risky t
+  :type '(choice
+          (file :tag "Filename (default is \"imandra-lsp\")")
+          (const :tag "Use current opam switch" opam)))
+
+(defun imandra--lsp-command ()
+  "Generate LSP startup command for the Imandra Language Server."
+  (cond
+   ((stringp imandra-lsp-command) imandra-lsp-command)
+   ((equal imandra-lsp-command 'opam) (list "opam" "exec" "--" "imandra-lsp"))))
+
+(add-to-list 'lsp-language-id-configuration '(imandra-mode . "imandra"))
+
+(lsp-register-client
+ (make-lsp-client
+  :new-connection (lsp-stdio-connection #'imandra--lsp-command)
+  :major-modes '(imandra-mode)
+  :server-id 'imandra-lsp))
--- imandra-mode.el
+++ imandra-mode.el
@@ -6,37 +6,9 @@
 ;; Version: 0.1
 ;; URL: http://github.com/aestheticintegration/imandra-mode
 
-(require 'lsp-mode)
 (require 'tuareg)
 
 ;;;###autoload
 (define-derived-mode imandra-mode tuareg-mode "Imandra")
-
-(defgroup imandra-lsp nil
-  "LSP support for Imandra"
-  :group 'lsp-mode)
-
-(defcustom imandra-lsp-command
-  "imandra-lsp"
-  "Path to the Imandra language server binary"
-  :group 'imandra-lsp
-  :risky t
-  :type '(choice
-          (file :tag "Filename (default is \"imandra-lsp\")")
-          (const :tag "Use current opam switch" opam)))
-
-(defun imandra--lsp-command ()
-  "Generate LSP startup command for the Imandra Language Server."
-  (cond
-   ((stringp imandra-lsp-command) imandra-lsp-command)
-   ((equal imandra-lsp-command 'opam) (list "opam" "exec" "--" "imandra-lsp"))))
-
-(add-to-list 'lsp-language-id-configuration '(imandra-mode . "imandra"))
-
-(lsp-register-client
- (make-lsp-client
-  :new-connection (lsp-stdio-connection #'imandra--lsp-command)
-  :major-modes '(imandra-mode)
-  :server-id 'imandra-lsp))
 
 (provide 'imandra-mode)
